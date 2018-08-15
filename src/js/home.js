327c4b0fbfd1b63228ade2b7d63cc39c0aa280a0
Orange <kai.cheng01@weimob.com>
2018-08-15 11:27:05 +0800

    将之前的项目添加到工程中，增加首页跳转

---
+++ src/js/home.js
@@ -0,0 +1,19 @@
+
+
+function projectListAction(index) {
+    switch (index)
+    {
+        case 1 :
+        {
+            window.location.href = "./src/pages/project1/src/main.html";
+        }
+        break;
+        case 2 :
+        {
+            window.location.href = "./src/pages/project1/src/classify.html";
+        }
+            break;
+        default:
+            break;
+    }
+}
